99266ad9610af1986dbf7bd4b2f94989010ea31d
ad0hr <127736969+ad0hr@users.noreply.github.com>
2023-03-14 01:10:08 +1300

    Create test_ttt.py

---
+++ test_ttt.py
@@ -0,0 +1,7 @@
+import pytest
+from functions_ttt import *
+
+def test_check_game_win_tricky_board():
+  board = [’O’, ’O’, ’X’, ’O’, ’X’, ’X’, ’X’, ’.’, ’.’]
+  is_win, player = check_game_win(board)
+  assert(player == ’X’)
