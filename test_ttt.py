5f8c2f31040b1a67264bc14a9ca89f0e3525ba6c
ad0hr <127736969+ad0hr@users.noreply.github.com>
2023-03-14 21:39:52 +1300

    Delete test_ttt.py

--- test_ttt.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from functions_ttt import *
-
-
-def test_check_game_win_tricky_board():
-    board = ['O', 'O', 'X', 'O', 'X', 'X', 'X', '.', '.']
-    is_win, player = check_win(board)
-    assert (player == 'X')
