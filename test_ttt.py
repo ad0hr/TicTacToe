3008566434879eac85cfbee78853d713e8d48549
ad0hr <127736969+ad0hr@users.noreply.github.com>
2023-03-14 20:34:05 +1300

    Add files via upload

--- test_ttt.py
+++ test_ttt.py
@@ -1,7 +1,8 @@
 import pytest
 from functions_ttt import *
 
+
 def test_check_game_win_tricky_board():
-  board = [’O’, ’O’, ’X’, ’O’, ’X’, ’X’, ’X’, ’.’, ’.’]
-  is_win, player = check_game_win(board)
-  assert(player == ’X’)
+    board = ['O', 'O', 'X', 'O', 'X', 'X', 'X', '.', '.']
+    is_win, player = check_win(board)
+    assert (player == 'X')
