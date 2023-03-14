0e3b0b6e62be5abb7042da659b7483eb649ae1af
Amanda <alee598@aucklanduni.ac.nz>
2023-03-14 23:21:47 +1300

    edits to the code

--- functions_ttt.py
+++ functions_ttt.py
@@ -1,17 +1,33 @@
 def initialise_board():
     """ makes an empty 3x3 table to use as the tic-tac-toe board
-        Arguments:
-        Returns: board_list (list), contains 9 strings of . to represent an empty 3x3 board
-        Notes: no pre- or post-conditions required."""
+-------------------------------------------------------------------------------------------------
+        Returns
+-------------------------------------------------------------------------------------------------
+        board_list
+        data type list
+        contains 9 strings of . to represent an empty 3x3 board
+-------------------------------------------------------------------------------------------------
+        Notes
+-------------------------------------------------------------------------------------------------
+        no pre- or post-conditions required."""
     board_list = ['.', '.', '.', '.', '.', '.', '.', '.', '.']
     return board_list
 
 
 def display_board(board_list):
     """ displays the 3x3 tic-tac-toe board
-        Arguments: board_list (list), holds the list of characters in the board
-        Returns:
-        Notes: the function will display whatever the board input is, so recent changes are taken into consideration"""
+-------------------------------------------------------------------------------------------------
+        Arguments
+-------------------------------------------------------------------------------------------------
+        board_list
+        of data type list
+        holds the list of characters in the board
+-------------------------------------------------------------------------------------------------
+        Returns
+-------------------------------------------------------------------------------------------------
+        Notes
+-------------------------------------------------------------------------------------------------
+        the function will display whatever the board input is, so recent changes are taken into consideration"""
     print(board_list[0:3])
     print(board_list[3:6])
     print(board_list[6:9])
@@ -19,9 +35,22 @@ def display_board(board_list):
 
 def get_current_turn_number(board_list):
     """ uses the board to determine the number of turns
-        Arguments: board_list (list), holds the list of characters in the board
-        Returns: turn_number (integer), represents the current number of turns
-        Notes: goes through the entire board list one by one to count the number of turns"""
+-------------------------------------------------------------------------------------------------
+        Arguments
+-------------------------------------------------------------------------------------------------
+        board_list
+        of data type list
+        holds the list of characters in the board
+-------------------------------------------------------------------------------------------------
+        Returns
+-------------------------------------------------------------------------------------------------
+        turn_number
+        of data type integer
+        represents the current number of turns
+-------------------------------------------------------------------------------------------------
+        Notes
+-------------------------------------------------------------------------------------------------
+        goes through the entire board list one by one to count the number of turns"""
     turn_number = 1
     for current_turn_number in board_list:
         if current_turn_number != '.':
@@ -31,10 +60,25 @@ def get_current_turn_number(board_list):
 
 def get_current_player(board_list):
     """ uses the board to determine the current player
-        Arguments: board_list (list), holds the list of characters in the board
-        Returns: O (string), which represents that the current player is O
-                X (string), which represents that the current player is X
-        Notes: there are only two players, O and X and X will always be the first player"""
+-------------------------------------------------------------------------------------------------
+        Arguments
+-------------------------------------------------------------------------------------------------
+        board_list
+        of data type list
+        holds the list of characters in the board
+-------------------------------------------------------------------------------------------------
+        Returns
+-------------------------------------------------------------------------------------------------
+        'O'
+        of data type string
+        which represents that the current player is O
+        'X'
+        of data type string
+        which represents that the current player is X
+-------------------------------------------------------------------------------------------------
+        Notes
+-------------------------------------------------------------------------------------------------
+        there are only two players, O and X and X will always be the first player"""
     number_of_x = 1
     number_of_O = 0
     for current_turn in board_list:
@@ -52,18 +96,37 @@ def get_current_player(board_list):
 
 def play_turn(board_list, row, col):
     """ places a character on the board, using a row and column position
-        Argument: board_list (list), holds the list of characters in the board
-                row (int), row that the character will be placed onto on the board
-                col (int), column that the character will be placed onto on the board
-        Returns: True (boolean), if the turn is valid-the position that the character is to be placed on was
-                originally empty
-                False (boolean), to be returned if the turn is invalid-the new position the character is to be placed
-                on is taken
-                board_list (list),
-                if the turn is valid-the updated board with the addition of the new character
-                if turn is invalid-the same board as the input
-        Notes: Can only pick from rows 1-3 and columns 1-3 due to the 3x3 nature of board
-            uses the previous function get_current_player
+-------------------------------------------------------------------------------------------------
+        Argument
+-------------------------------------------------------------------------------------------------
+        board_list
+        of data type list
+        holds the list of characters in the board
+-------------------------------------------------------------------------------------------------
+        row
+        of data type integer
+        row that the character will be placed onto on the board
+        col
+        of data type integer
+        column that the character will be placed onto on the board
+-------------------------------------------------------------------------------------------------
+        Returns
+-------------------------------------------------------------------------------------------------
+        True
+        of data type boolean
+        if the turn is valid-the position that the character is to be placed on was originally empty
+        False
+        of data type boolean
+        to be returned if the turn is invalid-the new position the character is to be placed on is taken
+        board_list
+        of data type list
+        if the turn is valid-the updated board with the addition of the new character
+        if turn is invalid-the same board as the input
+-------------------------------------------------------------------------------------------------
+        Notes
+-------------------------------------------------------------------------------------------------
+        Can only pick from rows 1-3 and columns 1-3 due to the 3x3 nature of board
+        Uses the previous function get_current_player
         """
     row = int(row)
     col = int(col)
@@ -84,10 +147,25 @@ def play_turn(board_list, row, col):
 
 def check_draw(board_list):
     """ checks if there is a draw
-        Arguments: board_list (list), holds the list of characters in the board
-        Returns: True (boolean), if there is a draw
-                False (boolean), there is no draw
-        Notes: uses previous function get_current_turn_number"""
+-------------------------------------------------------------------------------------------------
+        Arguments
+-------------------------------------------------------------------------------------------------
+        board_list
+        of data type list
+        holds the list of characters in the board
+-------------------------------------------------------------------------------------------------
+        Returns
+-------------------------------------------------------------------------------------------------
+        True
+        of data type boolean
+        will be returned if there is a draw
+        False
+        of data type boolean
+        will be returned if there is no draw
+-------------------------------------------------------------------------------------------------
+        Notes
+-------------------------------------------------------------------------------------------------
+        uses previous function get_current_turn_number"""
     if get_current_turn_number(board_list) == 10:
         # if the number of turns gets to 10, all the places have been taken up with no winner, so there will
         # inevitably be a draw
@@ -98,65 +176,71 @@ def check_draw(board_list):
 
 def check_win(board_list):
     """ checks if there is a win in the board, and which player was the winner
-        Arguments: board_list (list), holds the list of characters in the board
-        Returns: True (boolean), there is a win in the board
-                False (boolean), there is no win in the board
-                O (string), the winner is player O
-                X (string), the winner is player X
-        Notes: function checks for all possible winning combinations and returns if the winning combination is met"""
+-------------------------------------------------------------------------------------------------
+        Arguments
+-------------------------------------------------------------------------------------------------
+        board_list
+        of data type list
+        holds the list of characters in the board
+-------------------------------------------------------------------------------------------------
+        Returns
+-------------------------------------------------------------------------------------------------
+        True
+        of data type boolean
+        returned if there is a win in the board
+        False
+        of data type boolean
+        returned if there is no win in the board
+        O
+        of data type string
+        returned if the winner is player O
+        X
+        of data type string
+        returned if the winner is player X
+-------------------------------------------------------------------------------------------------
+        Notes
+-------------------------------------------------------------------------------------------------
+        function checks for all possible winning combinations and returns if the winning combination is met"""
     # while loop goes through every element in board_list
-    if ((board_list[0:3]) or (board_list[3:6]) or (board_list[6:9])) == ['X', 'X', 'X']:
-        # checking for X win across all rows
-        return True, 'X'
-    elif ((board_list[0:3]) or (board_list[3:6]) or (board_list[6:9])) == ['O', 'O', 'O']:
-        # checking for O win across all rows
+    if (board_list[0] and board_list[1] and board_list[2] == 'O') or \
+            (board_list[3] and board_list[4] and board_list[5] == 'O') or \
+            (board_list[6] and board_list[7] and board_list[8] == 'O') or \
+            (board_list[0] and board_list[3] and board_list[6] == 'O') or \
+            (board_list[1] and board_list[4] and board_list[7] == 'O') or \
+            (board_list[2] and board_list[5] and board_list[8] == 'O') or \
+            (board_list[0] and board_list[4] and board_list[8] == 'O') or \
+            (board_list[2] and board_list[4] and board_list[6] == 'O'):
+        print(board[6], board[7], board[])
         return True, 'O'
-    elif board_list[0] == 'X' and board_list[3] == 'X' and board_list[6] == 'X':
-        # checking for X win down leftmost column
+    elif (board_list[0] and board_list[1] and board_list[2] == 'X') or \
+            (board_list[3] and board_list[4] and board_list[5] == 'X') or \
+            (board_list[6] and board_list[7] and board_list[8] == 'X') or \
+            (board_list[0] and board_list[3] and board_list[6] == 'X') or \
+            (board_list[1] and board_list[4] and board_list[7] == 'X') or \
+            (board_list[2] and board_list[5] and board_list[8] == 'X') or \
+            (board_list[0] and board_list[4] and board_list[8] == 'X') or \
+            (board_list[2] and board_list[4] and board_list[6] == 'X'):
         return True, 'X'
-    elif board_list[1] == 'X' and board_list[4] == 'X' and board_list[7] == 'X':
-        # checking for X win down middle column
-        return True, 'X'
-    elif board_list[2] == 'X' and board_list[5] == 'X' and board_list[8] == 'X':
-        # checking for X win down rightmost column
-        return True, 'X'
-    elif board_list[0] == 'O' and board_list[3] == 'O' and board_list[6] == 'O':
-        # checking for O win down leftmost column
-        return True, 'O'
-    elif board_list[1] == 'O' and board_list[4] == 'O' and board_list[7] == 'O':
-        # checking for O win down middle column
-        return True, 'O'
-    elif board_list[2] == 'O' and board_list[5] == 'O' and board_list[8] == 'O':
-        # checking for O win down rightmost column
-        return True, 'O'
-    elif board_list[0] == 'X' and board_list[4] == 'X' and board_list[8] == 'X':
-        # checking for X win across the diagonals
-        return True, 'X'
-    elif board_list[2] == 'X' and board_list[4] == 'X' and board_list[6] == 'X':
-        # checking for X win across the diagonals
-        return True, 'X'
-    elif board_list[0] == 'O' and board_list[4] == 'O' and board_list[8] == 'O':
-        # checking for O win across the diagonals
-        return True, 'O'
-    elif board_list[2] == 'O' and board_list[4] == 'O' and board_list[6] == 'O':
-        # checking for O win across the diagonals
-        return True, 'O'
     else:
         return False, None
 
 
 def play_game():
     """ uses user input to play tic-tac-toe
-        Arguments:
-        Returns:
-        Notes: No arguments or returns as the function wholly depends on user input
-            This function uses all previous functions
-        Example: The function will print all beginning instructions, and initialise the board
-                It will only need a row and column input each 'turn' to change the board
-                It will also check for winning combinations and draws
-                So the game will announce a winner, or a draw when obtained
-                Game will also not allow invalid turns (new position has already been taken up by an O or X, will
-                instead display the previous unchanged board again"""
+-------------------------------------------------------------------------------------------------
+        Notes
+-------------------------------------------------------------------------------------------------
+        No arguments or returns as the function wholly depends on user input
+        This function uses all previous functions
+-------------------------------------------------------------------------------------------------
+        Example
+-------------------------------------------------------------------------------------------------
+        The function will print all beginning instructions, and initialise the board
+        It will only need a row and column input each 'turn' to change the board
+        It will also check for winning combinations and draws
+        So the game will announce a winner, or a draw when obtained
+        Game will also not allow invalid turns (new position has already been taken up by an O or X, will
+        instead display the previous unchanged board again"""
     print('Welcome to TIC TAC TOE!\n'
           'To play, type in the desired row and column of where you want to place your character\n'
           'Make sure you only play when it\'s your turn- Player X always goes first\n'
@@ -176,12 +260,12 @@ def play_game():
         print('Column number:')
         col = input()
         play_turn(board, row, col)
-        win, winner = check_win(board)
-        if winner == 'X':
+        is_win, winner = check_win(board)
+        if is_win and (winner == 'X'):
             display_board(board)
             print('Player X has won!')
             return
-        elif winner == 'O':
+        elif is_win and (winner == 'O'):
             display_board(board)
             print('Player O has won!')
             return
